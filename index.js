9c3063f36f74ee9fe227d6dee7ea25709955cc24
bineetNaidu <bineet.s.naidu@gmail.com>
2020-08-20 23:08:40 +0530

    -- display simple sign form to display  -- POST / GET to root route

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const app = require("express")();
 
-app.get("/", (req, res) => res.send(`hi there`));
+app.get("/", (req, res) => {
+    res.send(`
+  <div>
+    <form method="POST">
+      <input name="email" placeholder="email" />
+      <input name"password" placeholder="password" />
+      <input name="passwordConfirmation" placeholder="Confirm password" />
+      <button>Summit</button>
+    </form>
+  </div>
+`);
+});
+
+app.post("/", (req, res) => console.log("Account Created!"));
 
 app.listen(process.env.PORT || 3000, () => console.log("Server started"));
